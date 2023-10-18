228d0ceeffaf624cddc1365012d59ea399ee64d4
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-10-18 21:07:34 +0200

    fix(open): consistent behaviour in/out of tmux

--- src/core.rs
+++ src/core.rs
@@ -1,31 +1,36 @@
 use std::{env, path::PathBuf};
 
 use anyhow::{bail, Result};
-use tmux_interface::{HasSession, NewSession, NewWindow, SelectWindow, SwitchClient, Tmux, AttachSession};
+use tmux_interface::{AttachSession, HasSession, NewSession, NewWindow, SwitchClient, Tmux};
 
 pub fn in_tmux() -> bool {
     env::var("TMUX").is_ok()
 }
 
 pub fn open(path: &PathBuf) -> Result<()> {
-    let session_name = path
-        .file_name()
-        .map(|s| s.to_str())
-        .flatten()
-        .unwrap_or("unnamed");
-
-    Tmux::with_command(AttachSession::new()).output()?;
+    let session_name = session_name(path);
 
     let session_exists = Tmux::with_command(HasSession::new().target_session(session_name))
         .output()?
         .status()
         .success();
 
-    // Attach to existing session, if possible
-    if session_exists {
-        Tmux::with_command(SwitchClient::new().target_session(session_name)).output()?;
-        return Ok(());
+    if !session_exists {
+        create_session(path)?;
     }
+
+    // Attach to session
+    if in_tmux() {
+        Tmux::with_command(SwitchClient::new().target_session(session_name)).output()?;
+    } else {
+        Tmux::with_command(AttachSession::new().target_session(session_name)).output()?;
+    }
+
+    Ok(())
+}
+
+fn create_session(path: &PathBuf) -> Result<()> {
+    let session_name = session_name(path);
 
     // Create new session
     // IDEA: Have this run in Lua
@@ -33,13 +38,21 @@ pub fn open(path: &PathBuf) -> Result<()> {
         .add_command(
             NewSession::new()
                 .session_name(session_name)
-                .shell_command("zsh -c nvim"),
+                .shell_command("zsh -c nvim")
+                .detached(),
         )
-        .add_command(NewWindow::new())
-        .add_command(SelectWindow::new().target_window("1"));
+        .add_command(NewWindow::new().detached());
     let res = cmd.status()?;
     if !res.success() {
         bail!("tmux exited with non-zero status");
     }
+
     Ok(())
+}
+
+fn session_name(path: &PathBuf) -> &str {
+    path.file_name()
+        .map(|s| s.to_str())
+        .flatten()
+        .unwrap_or("unnamed")
 }
